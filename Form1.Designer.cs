c604c7ea1f4481b438efcb5a4421de63fdbba4f0
Mevlut BOZTEPE <125694277+mevlutboztepe@users.noreply.github.com>
2023-05-25 11:38:11 +0300

    ITS_Datamatrix_Info

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Unikod_TEST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.GTIN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.serial_number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.skt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lot_number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_count = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(493, 340);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(548, 36);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(357, 340);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(678, 831);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ÇALIŞTIR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Karekod :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(544, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Seri Numarası :";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.GTIN,
+            this.serial_number,
+            this.skt,
+            this.lot_number});
+            this.dataGridView1.Location = new System.Drawing.Point(30, 491);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(875, 334);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // GTIN
+            // 
+            this.GTIN.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.GTIN.HeaderText = "GTIN";
+            this.GTIN.MinimumWidth = 6;
+            this.GTIN.Name = "GTIN";
+            this.GTIN.ReadOnly = true;
+            // 
+            // serial_number
+            // 
+            this.serial_number.HeaderText = "Seri Numarası";
+            this.serial_number.MinimumWidth = 6;
+            this.serial_number.Name = "serial_number";
+            this.serial_number.ReadOnly = true;
+            this.serial_number.Width = 170;
+            // 
+            // skt
+            // 
+            this.skt.HeaderText = "Son Kullanma Tarihi";
+            this.skt.MinimumWidth = 6;
+            this.skt.Name = "skt";
+            this.skt.ReadOnly = true;
+            this.skt.Width = 170;
+            // 
+            // lot_number
+            // 
+            this.lot_number.HeaderText = "Lot Numarası";
+            this.lot_number.MinimumWidth = 6;
+            this.lot_number.Name = "lot_number";
+            this.lot_number.ReadOnly = true;
+            this.lot_number.Width = 170;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(27, 379);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(212, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "*Lütfen Kare Kod\'ları Alt Alta Giriniz!";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 405);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = " Girilen Kare Kod Adedi : ";
+            // 
+            // lbl_count
+            // 
+            this.lbl_count.AutoSize = true;
+            this.lbl_count.Location = new System.Drawing.Point(192, 405);
+            this.lbl_count.Name = "lbl_count";
+            this.lbl_count.Size = new System.Drawing.Size(14, 16);
+            this.lbl_count.TabIndex = 7;
+            this.lbl_count.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(26, 457);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(160, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Karekod Bilgileri :";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(436, 831);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(227, 50);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "TEMİZLE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 893);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbl_count);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Karekod Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GTIN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn serial_number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn skt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lot_number;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_count;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unikod_TEST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        public void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                textBox2.Clear();               
+                dataGridView1.Rows.Clear();
+                List<string> textboxlines = new List<string>(textBox1.Lines);
+                string gtin, seri_number, skt, lot_number;
+                List<string> serialnumberlist = new List<string>();
+
+                foreach (var lines in textboxlines)
+                {
+                    
+                    if (lines == null || lines == "")
+                    {
+
+                        continue;
+                    }
+
+                    else if (lines.Trim().Substring(0, 2) == "01" && lines.Trim().Substring(16, 2) == "21" && lines.Trim().Substring(33, 2) == "17" && lines.Trim().Substring(41, 2) == "10")
+                    {
+
+                        // 01 08699514092476 21 260734000995099 17 230228 10 21C623   
+                        // 01 05909991439705 21 HKVP108EF36TYK 17 241031 10 21L378
+                        gtin = lines.Substring(2, 14).ToString();
+                        seri_number = lines.Substring(18, 15).ToString();
+                        skt = lines.Substring(39, 2).ToString() + "/" + lines.Substring(37, 2).ToString() + "/" + lines.Substring(35, 2).ToString();
+                        lot_number = lines.Substring(43, 6).ToString();
+                        serialnumberlist.Add(seri_number);
+                        string[] bilgiler = { gtin, seri_number, skt, lot_number };
+                        dataGridView1.Rows.Add(bilgiler);
+
+
+                    }
+                    else if (lines.Trim().Substring(0, 2) == "01" && lines.Trim().Substring(16, 2) == "21" && lines.Trim().Substring(34, 2) == "17" && lines.Trim().Substring(42, 2) == "10")
+                    {
+                        gtin = lines.Substring(2, 14).ToString();
+                        seri_number = lines.Substring(18, 16).ToString();
+                        skt = lines.Substring(40, 2).ToString() + "/" + lines.Substring(38, 2).ToString() + "/" + lines.Substring(36, 2).ToString();
+                        lot_number = lines.Substring(44, 6).ToString();
+                        serialnumberlist.Add(seri_number);
+                        string[] bilgiler = { gtin, seri_number, skt, lot_number };
+                        dataGridView1.Rows.Add(bilgiler);
+
+                    }
+                    else if (lines.Trim().Substring(0, 2) == "01" && lines.Trim().Substring(16, 2) == "21" && lines.Trim().Substring(32, 2) == "17" && lines.Trim().Substring(40, 2) == "10")
+                    {
+                        gtin = lines.Substring(2, 14).ToString();
+                        seri_number = lines.Substring(18, 14).ToString();
+                        skt = lines.Substring(38, 2).ToString() + "/" + lines.Substring(36, 2).ToString() + "/" + lines.Substring(34, 2).ToString();
+                        lot_number = lines.Substring(42, 6).ToString();
+                        serialnumberlist.Add(seri_number);
+                        string[] bilgiler = { gtin, seri_number, skt, lot_number };
+                        dataGridView1.Rows.Add(bilgiler);
+
+                    }
+
+
+                    else
+                    {
+                        MessageBox.Show(lines + "Kare Kodu Hatalı");
+                    
+                    }
+                }
+                foreach (var item in serialnumberlist)
+                {
+                    textBox2.AppendText(item + Environment.NewLine);
+                }
+
+                lbl_count.Text = serialnumberlist.Count.ToString();
+           
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            dataGridView1.Rows.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
